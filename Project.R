df9988303c68c0b202a32dd67dde0f87c4aabfec
MaxS04 <maxsisneros04@gmail.com>
2026-03-31 14:59:17 -0600

    Cleaned up data. Added a plot that shows distributions of elevations by county

--- Project.R
+++ Project.R
@@ -11,6 +11,17 @@ hurricaneHWM <- readxl::read_excel(here("HurricaneData", "Hurricane_Helene_HWM_D
 hurricaneLS <- read_csv(here("HurricaneData", "HurricaneHelene_LS_Inventory.csv"))
 
 HWMbyHeight <- hurricaneHWM %>% arrange(-hwm_elevation_ft) %>%
-  select(hwm_type, northing, easting, county, state)
+  select(hwm_type, hwm_elevation_ft, northing, easting, county, state)
+
+HWMbyCounty <- hurricaneHWM %>% arrange(-hwm_elevation_ft) %>%
+  select(hwm_type, hwm_elevation_ft, northing, easting, county, state)
 
 HWMbyHeight %>% count(state, sort = TRUE)
+HWMbyCounty %>% count(county, sort = TRUE)
+
+
+#plotting data 
+library(ggplot2)
+
+ggplot(HWMbyCounty, aes(x = county, y = hwm_elevation_ft)) +
+  geom_point() + theme(axis.text.x = element_text(angle = 45, hjust = 1))
